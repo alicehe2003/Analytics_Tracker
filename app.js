e7d97cb5e0018d69c256b0316be1373999da7c87
alicehe2003 <140996846+alicehe2003@users.noreply.github.com>
2024-10-22 16:15:50 -0700

    Implement password encryption

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const session = require("express-session"); 
 const passport = require("passport"); 
 const LocalStrategy = require('passport-local').Strategy; 
+const bcrypt = require('bcryptjs'); 
 
 const pool = new Pool({
     // PostgressSQL config 
@@ -18,7 +19,10 @@ passport.use(
             if (!user) {
                 return done(null, false, { message: "Incorrect username" }); 
             }
-            if (user.passport !== passport) {
+
+            const match = await bcrypt.compare(password, user.password); 
+            if (!match) {
+                // passwords do not match 
                 return done(null, false, { message: "Incorrect password" }); 
             }
             return done(null, user); 
@@ -69,11 +73,19 @@ app.get("/log-out", (req, res, next) => {
 
 app.post("/sign-up", async (req, res, next) => {
     try {
-        await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
-            req.body.username, 
-            req.body.passport, 
-        ]); 
-        res.redirect("/"); 
+        // hash password before saving 
+        bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+            if (err) {
+                return next(err); 
+            }
+
+            // store hashed password 
+            await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
+                req.body.username, 
+                hashedPassword, 
+            ]); 
+            res.redirect("/"); 
+        }); 
     } catch (err) {
         return next(err); 
     }
